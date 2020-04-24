4034e6117199613aa5dc1c78103b28d832843960
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2020-04-24 14:19:16 +0530

    postman put update delete push

--- jpaproject/src/main/java/com/example/demo/AlienController.java
+++ jpaproject/src/main/java/com/example/demo/AlienController.java
@@ -1,21 +1,24 @@
 package com.example.demo;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-
 
 import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
-@Controller
+@RestController
 public class AlienController {
 	@Autowired
 	AlienRepo arep;
@@ -24,22 +27,34 @@ public class AlienController {
 	public String home() {
 		return "home.jsp";
 	}
-	
-//	@RequestMapping("/addAlien")
-//	public String addAlien(Alien alien) {
-//		arep.save(alien);
-//		return "home.jsp";
-//	}
-	@RequestMapping("/aliens")
-	@ResponseBody
+
+	@PostMapping("/alien")
+	public Alien addAlien(@RequestBody Alien alien) {
+		arep.save(alien);
+		return alien;
+	}
+
+	@DeleteMapping("/alien")
+	public String deleteAlien(@PathVariable int aid) {
+		Alien a = arep.getOne(aid);
+		arep.delete(a);
+		return "deleted";
+	}
+
+	@PutMapping("/alien")
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
+		arep.save(alien);
+		return alien;
+	}
+	@GetMapping(path = "/aliens", produces = { "applications/xml" })
 	public List<Alien> getAliens() {
-		
+
 		return arep.findAll();
 	}
+
 	@RequestMapping("/alien/{aid}")
-	@ResponseBody
 	public java.util.Optional<Alien> getAlien(@PathVariable("aid") int aid) {
-		
+
 		return arep.findById(aid);
 	}
 }
