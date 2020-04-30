6d036b785114b20e631a4868aa785f4b3f7bfa68
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2020-04-30 15:10:20 +0530

    trail with mysqlworkbench failure

--- MyfirstWebApp-1/src/main/java/com/example/demo/TicketController.java
+++ MyfirstWebApp-1/src/main/java/com/example/demo/TicketController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class TicketController {
 	@Autowired
 	private Ticketdao dao;
 	@PostMapping("/booktickets")
-	public String bookTicket(List<Ticket> ticket) {
+	public String bookTicket(@RequestBody List<Ticket> ticket) {
 		dao.saveAll(ticket);
 		return "tickets saved";
 	}
