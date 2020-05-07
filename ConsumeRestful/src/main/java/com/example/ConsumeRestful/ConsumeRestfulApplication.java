fe39a60fb7285013ad3afcc56d04351d01ea9dc1
shanmukhsurendra <shanmukhsurendra@msitprogram.net>
2020-05-07 22:57:41 +0530

    consumer rest

---
+++ ConsumeRestful/src/main/java/com/example/ConsumeRestful/ConsumeRestfulApplication.java
@@ -0,0 +1,34 @@
+package com.example.ConsumeRestful;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumeRestfulApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsumeRestfulApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeRestfulApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+}
---
+++ ConsumeRestful/src/test/java/com/example/ConsumeRestful/ConsumeRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConsumeRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
